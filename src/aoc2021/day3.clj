3d7eecf15deb720c21914684361a90577d294c14
David Goeke <dg@github.dgoeke.io>
2021-12-03 08:26:00 -0800

    Day 3

---
+++ src/aoc2021/day3.clj
@@ -0,0 +1,44 @@
+(ns aoc2021.day3
+  (:require [clojure.java.io :as io]
+            [clojure.string :as str]))
+
+(def input (->> "day3.txt" io/resource slurp str/split-lines))
+
+(defn ones-complement [s]
+  (apply str (map {\0 \1, \1 \0} s)))
+
+(defn swap-map [x]   ; convert {:a 1 :b 1} to {:b 1 :a 1} so min-key works right
+  (->> (vec x)
+       (sort-by first)
+       reverse
+       (into {})))
+
+(defn common-bits [swap-fn key-fn input]
+  (->> (apply mapv vector input)           ; transpose the matrix
+       (map frequencies)
+       (map swap-fn)
+       (map (comp key (partial apply key-fn val)))  ; find key with largest (or smallest) value
+       (apply str)))
+
+(def most-common-bits (partial common-bits identity max-key))
+(def least-common-bits (partial common-bits swap-map min-key))
+
+(defn mult-binary-strings [str1 str2]
+  (* (Integer/parseInt str1 2) (Integer/parseInt str2 2)))
+
+(def part1
+  (mult-binary-strings (most-common-bits input)
+                       (ones-complement (most-common-bits input))))
+
+(defn rating-finder [data filter-fn]
+  (loop [bit-number 0
+         data data]
+    (let [common-bits (filter-fn data)
+          new-data (filter #(= (get % bit-number) (get common-bits bit-number)) data)]
+      (if (nil? (second new-data))
+        (first new-data)
+        (recur (inc bit-number) new-data)))))
+
+(def part2
+  (mult-binary-strings (rating-finder input most-common-bits)
+                       (rating-finder input least-common-bits)))
