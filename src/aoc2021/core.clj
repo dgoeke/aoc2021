968146e013bb6d727131066798acbb5a1c6fdd8a
David Goeke <dg@github.dgoeke.io>
2021-12-14 14:30:03 -0800

    Day 14

--- src/aoc2021/core.clj
+++ src/aoc2021/core.clj
@@ -1,7 +1,10 @@
 (ns aoc2021.core
+  (:require [clojure.java.io :as io]
+            [clojure.string :as str])
   (:gen-class))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(defn day [^Integer n]
+  (->> (format "day%d.txt" n)
+       io/resource
+       slurp
+       str/split-lines))
---
+++ src/aoc2021/day14.clj
@@ -0,0 +1,44 @@
+(ns aoc2021.day14
+  (:require [aoc2021.core :refer [day]]
+            [clojure.string :as str]))
+
+(defn parse-input [data]
+  (let [[template _ & rule-text] data
+        final-character (last template)
+        parsed-input (->> template (partition 2 1) frequencies)
+        rules (into {} (->> rule-text
+                            (map #(str/split % #" "))
+                            (map (fn [[[ch1 ch2] _ [target]]]
+                                   {[ch1 ch2] [[ch1 target] [target ch2]]}))))]
+    {:input parsed-input
+     :rules rules
+     :output-state {[final-character] 1}}))
+
+(defn apply-rules-once
+  [rules output-state input]
+  (->> input
+       (mapcat (fn [[pair count]]
+                 (map #(hash-map % count) (rules pair))))
+       (apply merge-with + output-state)))
+
+(defn iterate-rules
+  [iterations {:keys [input rules output-state]}]
+  (nth (iterate (partial apply-rules-once rules output-state) input) iterations))
+
+(defn calc-difference [result]
+  (->> (reduce-kv (fn [acc [k _] v]
+                    (update acc k (fnil + 0) v))
+                  {} result)
+       vals
+       (apply (comp (partial apply -) (juxt max min)))))
+
+
+(def part1
+  (time (->> (parse-input (day 14))
+             (iterate-rules 10)
+             calc-difference)))           ; => 3.169542 msecs
+
+(def part2
+  (time (->> (parse-input (day 14))
+             (iterate-rules 40)
+             calc-difference)))           ; => 7.592375 msecs
